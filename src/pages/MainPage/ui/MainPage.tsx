0d5b1dbdd8987a8e4159fd5531ff36945186a8c7
Retover <lehasazonoff@mail.ru>
2023-09-07 23:11:33 +0300

    testing png and svg

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,11 +1,13 @@
 import { BugButton } from 'app/providers/ErrorBoundary';
-import SvgI from 'shared/assets/icons/rocketP.png';
+import SvgI from 'shared/assets/icons/rocket.svg';
+import imgT from 'shared/assets/icons/rocketP.png';
 
 const MainPage = () => (
     <div>
         <p>MainPage</p>
         <BugButton />
         <SvgI />
+        <img src={imgT} alt="w" />
     </div>
 );
 

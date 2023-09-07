259aa56b4b329275ff994a75b8de455d98e1a116
Retover <lehasazonoff@mail.ru>
2023-09-07 21:49:56 +0300

    fix build plugins

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -5,13 +5,18 @@ import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import { BuildOptions } from './types/config';
 
 export function buildPlugins({ paths, isDev }:BuildOptions): WebpackPluginInstance[] {
-  return [
+  const plugins = [
     new ProgressPlugin(),
     new HtmlWebpackPlugin({ template: paths.html }),
     new MiniCssExtractPlugin({
       filename: 'css/[name].[contenthash:8].css',
       chunkFilename: 'css/[name].[contenthash:8].css',
     }),
-    isDev && (new ReactRefreshWebpackPlugin(), new HotModuleReplacementPlugin()),
   ];
+
+  if (isDev) {
+    plugins.push(new ReactRefreshWebpackPlugin());
+    plugins.push(new HotModuleReplacementPlugin());
+  }
+  return plugins;
 }
